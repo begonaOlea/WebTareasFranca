0ac30f1a5ca5f51d6fad68e4011caf98dea2bd35
user <user@192.168.195.231>
2020-12-22 11:01:12 +0100

    Inicip de proyecto. Clases Usuario y Tarea

---
+++ src/main/java/com/tareas/model/Usuario.java
@@ -0,0 +1,81 @@
+package com.tareas.model;
+
+import java.util.Objects;
+
+public class Usuario {
+
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String contrasenia;
+
+    public Usuario(String nombre, String apellidos, String email, String contrasenia) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 13 * hash + Objects.hashCode(this.email);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Usuario other = (Usuario) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", email=" + email + ", contrasenia=" + contrasenia + '}';
+    }
+
+}
